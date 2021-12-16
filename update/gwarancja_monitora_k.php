6a725d085b7c0cfcaaff786559eb46a4cf269b4a
praktykiEDU <95857031+praktykiEDU@users.noreply.github.com>
2021-12-16 08:58:19 +0100

    Add files via upload

---
+++ update/gwarancja_monitora_k.php
@@ -0,0 +1,18 @@
+<?php
+            session_start();
+            require_once "config.php";
+            $Gwarancja_monitora = $_POST['gwarancja_monitora'];
+            $ID = $_POST['id'];
+            $sql = "UPDATE komputery SET Gwarancja_monitora='$Gwarancja_monitora' WHERE ID='$ID'";
+            if (mysqli_query($link, $sql)) 
+                {
+                    echo "<script type='text/javascript'>alert('Zaaktualizowano');</script>";
+                    echo "<script>window.location = 'http://localhost/Gdynska-Siec-Szkolna-main/#loaded'</script>";
+                } 
+            else 
+                {
+                    echo "<script type='text/javascript'>alert('Błąd w wpisywaniu');</script>";
+                    //echo "Error: " . $sql . "<br>" . mysqli_error($link);
+                    echo "<script>window.location = 'http://localhost/Gdynska-Siec-Szkolna-main/update_k.php'</script>";
+                } 
+        ?>
